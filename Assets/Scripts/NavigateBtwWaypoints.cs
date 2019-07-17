e168a3c2eaa91374906526c740a69c64d8d7c095
Aniketh <a.gondesi@student.saedubai.com>
2019-07-18 01:24:13 +0400

    Enemy move around uisng waypoints insted of navmesh.

---
+++ Assets/Scripts/NavigateBtwWaypoints.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateBtwWaypoints : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject[] waypoints;
+    [SerializeField] float followRange;              //the distence at which  the objet should start chasing the player
+    [SerializeField] float speed;                    // speed at whih the enmey moves
+    int index = 0;                                   // which waypoint it is at or moving to right now
+    float distance;                                  // distance between enemy and waypoint
+    float distanceBtwPlayer;                         // distance between enemy and player
+    Vector3 direction;                               // diretion to move in
+    bool inverse;                                    // swith teh movement diretions
+    bool chasePlayer;                                // to chase player or not
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = (waypoints[index].transform.position - transform.position).normalized;
+    }
+
+    
+    void Update()
+    {
+        //Setting to chase player or resetting enemy position if player gets away
+        distanceBtwPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if(distanceBtwPlayer < followRange)
+        {
+            chasePlayer = true;
+        }
+        else if(distanceBtwPlayer > followRange && chasePlayer)
+        {
+            chasePlayer = false;
+            index = 0;
+            direction = (waypoints[index].transform.position - transform.position).normalized;
+            transform.position = waypoints[index].transform.position;
+        }
+
+        // The movement between way points and set diretion to player if chasing
+        if (!chasePlayer)
+        {
+            distance = Vector3.Distance(transform.position, waypoints[index].transform.position);
+            if (distance < 0.2f)
+            {
+                if (!inverse)
+                {
+                    if (index >= waypoints.Length - 2)
+                    {
+                        inverse = true;
+                    }
+                    index++;
+                }
+                else
+                {
+                    if (index <= 1)
+                    {
+                        inverse = false;
+                    }
+                    index--;
+                }
+                direction = (waypoints[index].transform.position - transform.position).normalized;
+            }
+        }
+        else if(chasePlayer)
+        {
+            direction = (player.transform.position - transform.position).normalized;
+        }
+
+        transform.position += direction * speed * Time.deltaTime;                                      // moving to the current target
+    }
+}
