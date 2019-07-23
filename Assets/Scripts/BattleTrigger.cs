2079eb00ddacade11e1876680ff9f41290f4c308
Aniketh <a.gondesi@student.saedubai.com>
2019-07-23 08:45:41 +0400

    The Battale area is ready and the enemy will spawn in battle sceen when the player touches the enemy.

---
+++ Assets/Scripts/BattleTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject theOverWorld;
+    [SerializeField] GameObject battleArea;
+    [SerializeField] Transform enemyBattlePosition;
+    [SerializeField] GameObject enemyTypeToSpawn;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            transform.position = enemyBattlePosition.position;
+            theOverWorld.SetActive(false);
+            battleArea.SetActive(true);
+            Instantiate(enemyTypeToSpawn, enemyBattlePosition.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
